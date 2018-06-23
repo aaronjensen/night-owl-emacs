250824cbbdb37c631c9abc71fdf21cf35d476d8b
Aaron Jensen <aaronjensen@gmail.com>
2018-06-23 08:50:01 -0700

    Update description

--- night-owl-theme.el
+++ night-owl-theme.el
@@ -1,4 +1,4 @@
-;;; night-owl-theme.el --- A fruity color theme
+;;; night-owl-theme.el --- A color theme for the night owls out there
 
 ;; Copyright (C) 2018
 
@@ -22,7 +22,13 @@
 
 ;;; Commentary:
 ;;
-;; A port of the popular VSCode theme Night Owl.
+;; A port of the popular VS Code theme Night Owl. From the VS Code Night Owl README:
+;;
+;; A theme for the night owls out there. Works well in the daytime, too, but this theme is
+;; fine-tuned for those of us who like to code late into the night. Color choices have taken
+;; into consideration what is accessible to people with colorblindness and in low-light
+;; circumstances. Decisions were also based on meaningful contrast for reading comprehension
+;; and for optimal razzle dazzle.
 ;;
 ;;; Credits:
 ;;
